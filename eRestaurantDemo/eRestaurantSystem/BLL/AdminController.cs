20e706bb6f7bae24e8f733083ed911e2b12a809e
Don Welch <dwelch@nait.ca>
2015-10-14 20:58:09 -0600

    LVCRUD start of MessageUsercontrol

--- eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -18,6 +18,7 @@ namespace eRestaurantSystem.BLL
     [DataObject]
     public class AdminController
     {
+        #region Queries
         [DataObjectMethod(DataObjectMethodType.Select,false)]
         public List<SpecialEvent> SpecialEvents_List()
         {
@@ -113,7 +114,52 @@ namespace eRestaurantSystem.BLL
                 return results.ToList();
             }
         }
-       
-    }
-}
+        #endregion
+
+        #region Add, Update, Delete of CRUD for CQRS
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public void SpecialEvents_Add(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                //these methods are execute using an instance level item
+                //set up a instance pointer and initialize to null
+                SpecialEvent added = null;
+                //setup the command to execute the add
+                added = context.SpecialEvents.Add(item);
+                //command is not executed until it is actually saved.
+                context.SaveChanges();
+            }
+        }
+         [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void SpecialEvents_Update(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                //indicate the updating item instance
+                //alter the Modified Status flag for this instanc
+                context.Entry<SpecialEvent>(context.SpecialEvents.Attach(item)).State =
+                    System.Data.Entity.EntityState.Modified;
+                //command is not executed until it is actually saved.
+                context.SaveChanges();
+            }
+        }
+         [DataObjectMethod(DataObjectMethodType.Delete, false)]
+        public void SpecialEvents_Delete(SpecialEvent item)
+        {
+            using (eRestaurantContext context = new eRestaurantContext())
+            {
+                
+                //lookup the instance and record if found (set pointer to instance)
+                SpecialEvent existing = context.SpecialEvents.Find(item.EventCode);
+
+                //setup the command to execute the delete
+                context.SpecialEvents.Remove(existing);
+                //command is not executed until it is actually saved.
+                context.SaveChanges();
+            }
+        }
+        #endregion
+    }//eof class
+}//eof namespace
 
